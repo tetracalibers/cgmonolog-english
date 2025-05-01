18f590944e78138ec269f356bd77b26ae692fddf
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-05-01 18:09:18 +0900

    path aliasの設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config"
-
 import svelte from "@astrojs/svelte"
+
+import path, { dirname } from "node:path"
+import { fileURLToPath } from "node:url"
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  vite: {
+    resolve: {
+      alias: {
+        $: path.resolve(__dirname, "./src")
+      }
+    }
+  }
 })
