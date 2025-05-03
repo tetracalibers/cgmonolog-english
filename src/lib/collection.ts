232f1321597712dc28b934d5a99173a7e78f137f
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-05-03 18:36:44 +0900

    不要になった関数を削除

--- src/lib/collection.ts
+++
@@ -1,6 +0,0 @@
-import { getCollection } from "astro:content"
-
-export const getPieces = async () => {
-  const entries = await getCollection("piece")
-  return entries.sort((a, b) => a.data.index - b.data.index)
-}
